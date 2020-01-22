7d675cfd8f22c4d21f192a9c2ee17ac89c1dd605
unknown <taher.bouaine@gmail.com>
2020-01-22 11:41:33 +0100

    kjgh

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,7 +15,7 @@ public class Greeter {
    * @param someone this name of person
     * @return greeting String
    */
-  //TODO: Add javadoc comment
+
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
